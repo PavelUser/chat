71ec20d4f7c51886996108dd77e685cc63130981
Павел <>
2018-02-28 19:41:33 +0300

    Всё раскидано по методам (первый вариант)

--- src/com/company/server/ServerChat.java
+++ src/com/company/server/ServerChat.java
@@ -12,67 +12,89 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class ServerChat {
-    private static final Logger log = Logger.getLogger(ServerChat.class);
+    private final Logger log = Logger.getLogger(ServerChat.class);
 
-    static boolean work=true;
+    DataInputStream inStream;
+    DataOutputStream outstream;
+    final int port=2000;
+
+    boolean work=true;
 
     public static void main(String[] args){
 
-        final int port=2000;
-        InetAddress inetAddress=null;
+        ServerChat serverChat=new ServerChat();
+        serverChat.getIP();
+        serverChat.startWork();
+
+    }
+
+    private void getIP(){
 
         try {
-            inetAddress = InetAddress.getLocalHost();
+            log.info("ip address: "+InetAddress.getLocalHost().getHostAddress());
         }catch (UnknownHostException host){
-            host.printStackTrace();
+            log.error("IP сервера не получен", host);
         }
+    }
 
-        log.info("ip address(getLocalHost): " + inetAddress
-                +"\nЖдём подключения клиента\n");
+    private void handleMsg(DataInputStream inStream, DataOutputStream outstream, String str) throws IOException{
+
+        log.info("ждем сообщение от клиента\n");
+        str = inStream.readUTF();
+        log.info("Прислали строку: " + str+"\n");
+        log.info("Отпровляем обратно\n");
+        outstream.writeUTF(str);
+        outstream.flush();
+        log.info("Отправлена строка: " + str + "\n\n");
+    }
+
+    private void initDataStream(Socket socket) throws IOException{
+
+        inStream = new DataInputStream(socket.getInputStream());
+        outstream = new DataOutputStream(socket.getOutputStream());
+    }
+
+    private void startWork() {
 
         try (ServerSocket serverSocket = new ServerSocket(port);
              Socket socket = serverSocket.accept()) {
 
             log.info("Соединение с клиентом установлено\n");
 
-            DataInputStream inStream = new DataInputStream(socket.getInputStream());
-            DataOutputStream outstream = new DataOutputStream(socket.getOutputStream());
-
-            new Thread() {
-                private boolean running = true;
-
-                @Override
-                public void run() {
-                    Scanner scanner = new Scanner(System.in);
-
-                    do {
-                        if (scanner.nextInt() == 1)
-                            running = false;
-                    } while (running);
-
-                    work = false;
-                }
-            }.start();
-            String str;
+            initDataStream(socket);
+            keyboardHendler();
 
             log.info("Для выключения сервера введите 1\n\n");
+            String str=null;
 
             while (work) {
-
-                log.info("ждем сообщение от клиента\n");
-                str = inStream.readUTF();
-                log.info("Прислали строку: " + str+"\n");
-                log.info("Отпровляем обратно\n");
-                outstream.writeUTF(str);
-                outstream.flush();
-                log.info("Отправлена строка: " + str + "\n\n");
+                handleMsg(inStream,outstream,str);
             }
 
-            socket.close();
         }catch (IOException io){
             log.error("I/O exception",io);
         }
 
-        log.info("Сервер выключен\n");
+            log.info("Сервер выключен\n");
     }
+
+    private void keyboardHendler(){
+
+        new Thread() {
+            private boolean running = true;
+
+            @Override
+            public void run() {
+                Scanner scanner = new Scanner(System.in);
+
+                do {
+                    if (scanner.nextInt() == 1)
+                        running = false;
+                } while (running);
+
+                work = false;
+            }
+        }.start();
+    }
+
 }
