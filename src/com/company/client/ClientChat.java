37f9900b701fbc6241d7bd934be9bffd854c9fa5
Павел <>
2018-03-04 18:37:08 +0300

    Сервер поодерживает несколько клиентов одновременно. Нужно чуть изменить клиент и доделать закрытие потоков в сервере. log4j не пишет в файлы кроме info

--- src/com/company/client/ClientChat.java
+++ src/com/company/client/ClientChat.java
@@ -15,6 +15,8 @@ public class ClientChat{
     static final Logger log=Logger.getLogger(ClientChat.class);
 
     private final int port = 2000;
+    private boolean working=true;
+
     private DataOutputStream dataOutputStream;
     private DataInputStream dataInputStream;
     private Scanner scanner=new Scanner(System.in);
@@ -27,14 +29,10 @@ public class ClientChat{
     }
 
     private void startWork(){
-
-        log.info("Клиент запущен\n");
         log.info("Введите ip сервера: ");
-
         try (Socket socket = new Socket(enterIP(), port)){
             log.info("Соединение с сервером установлено"
-                    +"\nВведите сообщение\n");
-
+                    +"\nВведите сообщение");
             initDataStream(socket);
             inputMsgHandler(dataInputStream);
 
@@ -47,18 +45,16 @@ public class ClientChat{
         } catch (IOException e) {
             log.error("Ошибка I/O: ", e);
         }
-    }                                                   //отредактировать под новый код
+    }
 
     private String enterIP(){
-
         return scanner.nextLine();                                           //Переменная для чтения строки, ввденной с клавиатуры
     }
 
     private void initDataStream(Socket socket) throws IOException {
-
         dataOutputStream = new DataOutputStream(socket.getOutputStream());
         dataInputStream = new DataInputStream(socket.getInputStream());
-    }             //отредактировать под новый код
+    }
 
     private void inputMsgHandler(DataInputStream dataInputStream){
 
@@ -71,7 +67,7 @@ public class ClientChat{
 
                 try {
 
-                    while (true){
+                    while (working){
                         line = dataInputStream.readUTF();                   //прием сообщения от сервера
 
                         calendar=Calendar.getInstance();                        //Получаем время доставки
@@ -82,6 +78,7 @@ public class ClientChat{
 
                 } catch (EOFException ex){
                     log.error("Соединение с сервером потеряно\n", ex);
+                    System.out.println("Всё под контролем :)");
                     System.exit(1);
                 } catch (IOException ex){
                     log.error("Сообщение не может быть прочитано\n", ex);
@@ -90,7 +87,7 @@ public class ClientChat{
         };
         thread.setDaemon(true);
         thread.start();
-    }              //отредактировать под новый код
+    }
 
     private void sendMsg(String line, Calendar calendar) throws IOException {
 
@@ -100,6 +97,6 @@ public class ClientChat{
 
         System.out.printf("\t\t%tT \n%s", calendar,
                 "Отправлена строка: " + line);
-    }   //отредактировать под новый код
+    }
 
 }
--- src/com/company/server/ServerChat.java
+++ src/com/company/server/ServerChat.java
@@ -2,8 +2,11 @@ package com.company.server;
 
 import org.apache.log4j.Logger;
 
-import java.io.DataInputStream;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -15,22 +18,23 @@ import java.util.Scanner;
 public class ServerChat {
     private final Logger log = Logger.getLogger(ServerChat.class);
 
-    private DataInputStream inStream;
-    private DataOutputStream outstream;
-    final private int port=2000;
+    final static private int PORT=2000;
+    private boolean working=true;
+    private String msg;
 
-    private boolean work=true;
+    private ArrayList<Socket> clientlist=new ArrayList<>();
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private DataOutputStream dataOutputStream;
+    private ConnectionHandler connectionHandler;
+    private DataInputStream dataInputStream;
 
     public static void main(String[] args){
-
         ServerChat serverChat=new ServerChat();
-        serverChat.showIP();
         serverChat.startWork();
-
     }
 
     private void showIP(){
-
         try {
             log.info("ip address: "+InetAddress.getLocalHost().getHostAddress());
         }catch (UnknownHostException host){
@@ -38,73 +42,197 @@ public class ServerChat {
         }
     }
 
-    private void handleMsg(DataInputStream inStream, DataOutputStream outstream, String str) throws IOException{
-
-        log.info("ждем сообщение от клиента\n");
-        str = inStream.readUTF();
-        log.info("Прислали строку: " + str+"\n");
-        log.info("Отпровляем обратно\n");
-        outstream.writeUTF(str);
-        outstream.flush();
-        log.info("Отправлена строка: " + str + "\n\n");
+    private void showCommands(){
+        System.out.print("Список доступных команд:\n"+"exit\n"+"list\n");
     }
 
-    private void initDataStream(Socket socket) throws IOException{
+    private boolean isWorking(){
+        return working;
+    }
 
-        inStream = new DataInputStream(socket.getInputStream());
-        outstream = new DataOutputStream(socket.getOutputStream());
+    private void setWorking(boolean working){
+        this.working=working;
     }
 
     private void startWork() {
+        try {
+            serverSocket=new ServerSocket(PORT);
+            showIP();
+            connectionHandler=new ConnectionHandler();
+            showCommands();
+            commandHandler();
 
-        try (ServerSocket serverSocket = new ServerSocket(port);
-             /*Socket socket = serverSocket.accept()*/) {
-
-            //log.info("Соединение с клиентом установлено\n");
-
-            //initDataStream(socket);
-            keyboardHandler();
-
-            //Test
-            //----------------------------------------------------------------------------------------------------------
-            ConnectionHandler connectionHandler=new ConnectionHandler(serverSocket);
-            //----------------------------------------------------------------------------------------------------------
-
-            //log.info("Для выключения сервера введите 1\n\n");
-            //String str=null;
-
-            /*do{
-                //handleMsg(inStream,outstream,str);
+            while (isWorking()) {
+                Thread.sleep(1000);
             }
-            while (work);*/
-            connectionHandler.join();
+
+            connectionHandler.setRunning(false);
         }catch (IOException io){
             log.error("I/O exception",io);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
+        connectionHandler.closeAllSocket();
         log.info("Сервер выключен\n");
     }
 
-    private void keyboardHandler(){     //Нужно будет создать отдельный класс
+    private void commandHandler(){
 
         new Thread() {
             private boolean running = true;
+            private String command;
 
             @Override
             public void run() {
                 Scanner scanner = new Scanner(System.in);
 
                 do {
-                    if (scanner.nextInt() == 1) {
-                        running = false;
+                    command=scanner.nextLine().toLowerCase();
+                    switch (command){
+                        case "exit": {
+                            running = false;
+                            break;
+                        }
+                        case "list": {
+                            System.out.println("\tСписок клиентов");
+                            for (Socket client:clientlist)
+                                System.out.println(client.getInetAddress());
+                            break;
+                        }
+                        default:{
+                            System.out.println("Список доступных команд:\nexit\nlist");
+                            break;
+                        }
                     }
                 } while (running);
 
-                work = false;
+                setWorking(false);
             }
         }.start();
+    }
+
+    private class ConnectionHandler extends Thread {
+        final private Logger logger=Logger.getLogger(ConnectionHandler.class);
+
+        private boolean running=true;
+
+        ConnectionHandler(){
+            super("ConnectionHandler");
+            setDaemon(true);
+            start();
+        }
+
+        @Override
+        public void run(){
+            log.info("Поток "+getName()+ " Запущен");
+            while (isRunning()) {                                                           //не нравится выход из цикла нужно ждать подключения
+                try {
+                    socket = serverSocket.accept();                                         //Обрабатываем порт на наличие подключений
+                    logger.info("Соединение с клиентом установлено\n");
+                    clientlist.add(socket);                                                 //Добавляем клиента в список подключенных
+                    InputStreamHandler inputStreamHandler=new InputStreamHandler();         //Создаем по потоку на клиента
+                    OutputHandler outputStreamHandler=new OutputHandler();
+                    inputStreamHandler.addPropertyChangeListener(outputStreamHandler);
+                } catch (IOException ex) {
+                    logger.error(getName(), ex);
+                }
+            }
+            closeAllSocket();
+            logger.info("Остановлен поток: " + getName());
+        }
+
+        void closeAllSocket(){
+            for (Socket s:clientlist) {
+                try {
+                    s.close();
+                }catch (IOException io){
+                    logger.warn(s.getInetAddress().getHostAddress()+": сокет не закрыт",io);
+                }
+            }
+            logger.info("Все сокеты закрыты");
+        }
+
+        private boolean isRunning(){
+            return running;
+        }
+
+        void setRunning(boolean running){
+            this.running=running;
+        }
+
+    }
+
+    private class InputStreamHandler extends Thread{
+        private Logger logger=Logger.getLogger(InputStreamHandler.class);
+
+        private String ip;
+        private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+        InputStreamHandler(){
+            ip=socket.getInetAddress().getHostAddress();
+            setName("inputStreamHandler: "+ip);
+            try {
+                dataInputStream=new DataInputStream(socket.getInputStream());
+            } catch (IOException e) {
+                logger.error(getName(), e);
+            }
+            setDaemon(false);
+            start();
+        }
+
+        void addPropertyChangeListener(PropertyChangeListener listener) {
+            propertyChangeSupport.addPropertyChangeListener(listener);
+        }
+
+        @Override
+        public void run() {
+            logger.info("Поток "+getName()+" запущен");
+            try {
+                while (true) {                                                   //Подумать как организовать цикл
+                    logger.info("Принимаем сообщение");
+                    msg = dataInputStream.readUTF();                            //Принимаем сообщение
+                    logger.info("Принято сообщение: " + msg);
+                    propertyChangeSupport.firePropertyChange("msg", null, msg);
+                }
+            } catch (IOException e) {
+                logger.warn(getName()+" сообщение не принято",e);
+                try {
+                    dataInputStream.close();
+                    logger.info("Входный поток закрыт");
+                } catch (IOException e1) {
+                    logger.error("Входной поток "+ip+" не закрыт");
+                }
+            }
+        }
+    }
+
+    private class OutputHandler implements PropertyChangeListener{
+        private final Logger logger=Logger.getLogger(OutputHandler.class);
+
+        private String ip;
+
+        public OutputHandler() {
+            ip=socket.getInetAddress().getHostAddress();
+        }
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            for (Socket client:clientlist) {
+                try {
+                    dataOutputStream=new DataOutputStream(client.getOutputStream());
+                    dataOutputStream.writeUTF(msg);
+                } catch (IOException io) {
+                    logger.error("Сообщение: " + msg + " не отправлено");
+                    try {
+                        dataOutputStream.close();
+                        logger.info("Выходной поток закрыт");
+                    } catch (IOException e1) {
+                        logger.error("Выходной поток " + ip + " не закрыт");
+                    }
+                }
+            }
+            logger.info("Отправлено: " + msg);
+        }
     }
 
 }
